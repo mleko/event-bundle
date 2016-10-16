48ceffd5e69023d19d0d7d9ccd7656d89dfff0cc
Daniel Król <daniel@krol.me>
2016-10-16 17:52:25 +0200

    Add first test

---
+++ tests/Listener/ListenerServiceTest.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @package narrator-bundle
+ */
+
+namespace Mleko\Narrator\Bundle\Tests\Listener;
+
+class ListenerServiceTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var \Symfony\Component\DependencyInjection\ContainerInterface | \PHPUnit_Framework_MockObject_MockObject */
+    private $container;
+
+    public function setUp()
+    {
+        $this->container = $this->getMockForAbstractClass(\Symfony\Component\DependencyInjection\ContainerInterface::class);
+    }
+
+    public function testServiceRetrieval()
+    {
+        $service = $this->getMockBuilder(\stdClass::class)->setMethods(['handle'])->getMock();
+        $service->expects($this->once())->method('handle');
+
+        $this->container->expects($this->once())->method('get')->with('mock.listener')->willReturn($service);
+
+        $listener = new \Mleko\Narrator\Bundle\Listener\ListenerService('mock.listener', $this->container);
+
+        $event = new \stdClass();
+        $meta = new \Mleko\Narrator\Meta($event, 'strClass', 'stdClass', $this->getMockBuilder(\Mleko\Narrator\EventSource::class)->getMockForAbstractClass());
+        $listener->handle($event, $meta);
+    }
+}
